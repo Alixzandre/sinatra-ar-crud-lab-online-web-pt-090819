bc7d2aae50dc4ea8f69b67a964067a41cab2bc12
Alexander Bispo <alexbispo173@gmail.com>
2020-01-13 06:16:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
  
   post '/articles' do
     Article.create(params)
-    redirect '/articles'
+    redirect "/articles/#{}"
   end
   
   get '/articles' do 
@@ -26,20 +26,20 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/articles/:id' do 
-    @article = Article.find(params[:id])
+    @article = Article.find(params["id"])
     erb :show
   end
   
   get '/articles/:id/edit' do 
-    @article = Article.find(params[:id])
+    @article = Article.find(params["id"])
     erb :edit
   end
   
   patch '/articles/:id' do
     id = params["id"]
     new_params = {}
-    old_post = Post.find(id)
-    new_params[:name] = params["name"]
+    old_post = Article.find(id)
+    new_params[:title] = params["title"]
     new_params[:content] = params["content"]
     old_post.update(new_params)
 
